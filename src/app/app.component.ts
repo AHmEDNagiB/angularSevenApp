18b5b33ccafa59e88192303c2e28bc2cc47270c6
Ahmed Naguib <ahmed_naguib@siliconexpert.com>
2019-07-07 12:27:16 +0200

    Init commit , And basic for loop for log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angularSevenApp';
+  ngOnInit(): void {
+    for (let index = 1; index < 11; index++) {
+      console.log('index is ===> ', index);
+    }
+  }
 }
+
+
+
